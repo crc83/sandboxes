656463cd7edfbd7cdf33e82442dbe325fdd961e9
Sergiy Beley <sergiy.beley@gmail.com>
2023-01-18 19:49:16 +0200

    Checkzipkin generated project from start.spring.io

---
+++ checkzipkin/src/main/java/com/crc83/checkzipkin/CheckzipkinApplication.java
@@ -0,0 +1,13 @@
+package com.crc83.checkzipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckzipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckzipkinApplication.class, args);
+	}
+
+}
---
+++ checkzipkin/src/test/java/com/crc83/checkzipkin/CheckzipkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crc83.checkzipkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckzipkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
